4e2631d91a063382778bc6c8d635081e7f6fa16b
Sam Mason <sam.jbmason@gmail.com>
2014-02-15 16:52:14 +0000

    Add in new routes

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 class App < Sinatra::Application
-  get '/' do
 
+  get '/' do
+    erb :index
   end
 
   post '/' do
-
+    marvel = Marvel.new(ENV['MARVEL_PUBLIC_KEY'], ENV['MARVEL_PRIVATE_KEY'])
+    @characters = marvel.rand_characters
+    erb :index
   end
 end
