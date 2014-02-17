b89c8116e291badb694d26ac47c2f8cd7524ab07
Sam Mason <sam.jbmason@gmail.com>
2014-02-17 16:38:59 +0000

    Remove Post for now

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 class App < Sinatra::Application
 
   get '/' do
-    erb :index
-  end
-
-  post '/' do
     marvel = Marvel.new(ENV['MARVEL_PUBLIC_KEY'], ENV['MARVEL_PRIVATE_KEY'])
     @characters = marvel.rand_characters
     erb :index
+  end
+
+  helpers do
+    def hostname(text)
+      text.downcase.split(' (')[0].gsub(/\W+/, '_')
+    end
   end
 end
